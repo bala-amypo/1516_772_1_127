e7494ce62316107bcde7f89291ea32ff8325eca0
bala-amypo <bala@amypo.in>
2025-12-18 15:37:16 +0000

    User commit

---
+++ src/main/java/com/example/demo/entity/ComplaintStatus.java
@@ -0,0 +1,33 @@
+package com.example.demo.entity;
+
+import jakarta.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+public class ComplaintStatus {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    private Complaint complaint;
+
+    @Enumerated(EnumType.STRING)
+    private Status status;
+
+    private LocalDateTime updatedOn;
+
+    public enum Status {
+        OPEN, IN_PROGRESS, RESOLVED
+    }
+
+    @PrePersist
+    public void onUpdate() {
+        this.updatedOn = LocalDateTime.now();
+    }
+
+    public Status getStatus() {
+        return status;
+    }
+}
