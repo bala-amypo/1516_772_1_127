134214472742cf98b9cebc9b7a2a5faaaded3086
bala-amypo <bala@amypo.in>
2025-12-18 05:24:09 +0000

    User commit

--- src/main/java/com/example/demo/entity/Complaint.java
+++ src/main/java/com/example/demo/entity/Complaint.java
@@ -1,5 +1,4 @@
-package com.example.demo.entity
-
+package com.example.demo.entity;
 import jakarta.peristence.*;
 import java.time.LocalDateTime;
 @Entity
---
+++ src/main/java/com/example/demo/entity/PriorityRule.java
@@ -0,0 +1,10 @@
+package com.example.demo.entity;
+import jakarta.persistence.*;
+@Entity
+public class PriortyRule{
+    @Id
+    @GeneratedValue(Strategy= GenerationType.IDENTITY)
+    private Long id;
+    private String categry;
+    private 
+}
