7b813727114f78bfd7cdc8ac30f5b85ef448a286
bala-amypo <bala@amypo.in>
2025-12-18 15:23:04 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/ComplaintServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/ComplaintServiceImpl.java
@@ -1,12 +1,11 @@
 package com.example.demo.service.impl;
 
-import com.example.demo.dto.ComplaintRequest;
 import com.example.demo.entity.Complaint;
 import com.example.demo.entity.User;
+import com.example.demo.exception.ResourceNotFoundException;
 import com.example.demo.repository.ComplaintRepository;
 import com.example.demo.service.ComplaintService;
 import com.example.demo.service.PriorityRuleService;
-import com.example.demo.service.UserService;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -16,25 +15,20 @@ public class ComplaintServiceImpl implements ComplaintService {
 
     private final ComplaintRepository repo;
     private final PriorityRuleService ruleService;
-    private final UserService userService;
 
     public ComplaintServiceImpl(ComplaintRepository repo,
-                                PriorityRuleService ruleService,
-                                UserService userService) {
+                                PriorityRuleService ruleService) {
         this.repo = repo;
         this.ruleService = ruleService;
-        this.userService = userService;
     }
 
     @Override
-    public Complaint submitComplaint(ComplaintRequest r, User customer) {
-        Complaint c = new Complaint();
-        c.setCustomer(customer);
-        c.setSeverity(r.severity);
-        c.setUrgency(r.urgency);
-
-        c.setPriorityScore(ruleService.computePriorityScore(c));
-        return repo.save(c);
+    public Complaint submitComplaint(Complaint complaint, User customer) {
+        complaint.setCustomer(customer);
+        complaint.setPriorityScore(
+                ruleService.computePriorityScore(complaint)
+        );
+        return repo.save(complaint);
     }
 
     @Override
@@ -45,5 +39,16 @@ public class ComplaintServiceImpl implements ComplaintService {
     @Override
     public List<Complaint> getPrioritizedComplaints() {
         return repo.findAllOrderByPriorityScoreDescCreatedAtAsc();
+    }
+
+    @Override
+    public Complaint getComplaintById(Long id) {
+        return repo.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Complaint not found"));
+    }
+
+    @Override
+    public Complaint saveComplaint(Complaint complaint) {
+        return repo.save(complaint);
     }
 }
