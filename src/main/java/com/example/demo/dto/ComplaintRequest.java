fe265d2d12cf9c7c25726c0c9e301edd9ba713cc
bala-amypo <bala@amypo.in>
2025-12-25 16:02:13 +0000

    User commit

--- src/main/java/com/example/demo/dto/ComplaintRequest.java
+++ src/main/java/com/example/demo/dto/ComplaintRequest.java
@@ -3,7 +3,6 @@ package com.example.demo.dto;
 import com.example.demo.entity.Complaint;
 
 public class ComplaintRequest {
-
     private String title;
     private String description;
     private String category;
