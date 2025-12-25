02e4a7f5ef5df046f40c9c9e6f65f4961adbecb2
bala-amypo <bala@amypo.in>
2025-12-25 15:22:55 +0000

    User commit

--- src/main/java/com/example/demo/repository/ComplaintRepository.java
+++ src/main/java/com/example/demo/repository/ComplaintRepository.java
@@ -2,11 +2,15 @@ package com.example.demo.repository;
 
 import com.example.demo.entity.Complaint;
 import com.example.demo.entity.User;
+
 import java.util.List;
+import java.util.Optional;
 
 public interface ComplaintRepository {
 
     Complaint save(Complaint complaint);
+
+    Optional<Complaint> findById(Long id);
 
     List<Complaint> findByCustomer(User user);
 
