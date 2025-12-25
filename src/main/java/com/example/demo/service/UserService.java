3fe72d4f2c01d583f42de1f4b51c5335a46a4d76
bala-amypo <bala@amypo.in>
2025-12-25 16:11:19 +0000

    User commit

--- src/main/java/com/example/demo/service/UserService.java
+++ src/main/java/com/example/demo/service/UserService.java
@@ -4,7 +4,11 @@ import com.example.demo.entity.User;
 
 public interface UserService {
 
+    // used by tests
     User registerCustomer(String name, String email, String rawPassword);
-
     User findByEmail(String email);
+
+    // used by AuthController
+    User register(User user);
+    User login(String email, String password);
 }
